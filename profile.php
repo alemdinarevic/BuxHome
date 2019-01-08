3e39708b3f127f58550b5928b932a3d211f9509c
alemdinarevic <alemdinarevic@gmail.com>
2019-01-08 22:49:25 +0100

    changed frontend of profile.php

--- profile.php
+++ profile.php
@@ -10,12 +10,43 @@
 
 <?php include 'navbar.php';?>
 
-<div class="profile row">
+    <div class="profile">
 
-    <div class="profile-info col-md-3">
-        <img class="profile-pic" src="images/pp001.jpg">
+        <div class="profile-info">
+            <img class="profile-pic" src="images/pp002.jpg">
+        </div>
+
+        <div class="profile-posts">
+
+                <div class="row-book">
+                    <div class="info">
+                        <h1>Algorithms and data structures,<br>Thomas H. Cormen</h1>
+                        <p>This is the most popular book for developing
+                            and designing great algorithms.</p>
+                        <!--<p font-style="italic">Uploaded by: username. <br>17:09pm</p>-->
+                    </div>
+                    <div class="image">
+                        <image class="book-image" src="images/book1.png"></image>
+                    </div>
+                </div>
+
+
+            <div class="row-book">
+                <div class="info">
+                    <h1>1001 nights,<br>Various authors, scholars</h1>
+                    <p>One Thousand and One Nights  is
+                        a collection of Middle Eastern folk tales
+                        compiled in Arabic during the Islamic Golden Age.</p>
+                    <!--<p font-style="italic">Uploaded by: username. <br>17:09pm</p>-->
+                </div>
+                <div class="image">
+                    <image class="book-image" src="images/book002.png"></image>
+                </div>
+            </div>
+
+        </div>
+
     </div>
-</div>
 
 </body>
 </html>
